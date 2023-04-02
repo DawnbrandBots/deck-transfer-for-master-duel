161e925de0f75aa70ac98a13fd49a7acc11e3186
kevinlul <6320810+kevinlul@users.noreply.github.com>
2023-04-01 22:49:07 -0400

    More reliable deck builder shim with MutationObserver
    #8

--- integration.js
+++ integration.js
@@ -13,8 +13,8 @@ if (location.pathname === "/deckbuilder/") {
     if (ydke) {
         const script = document.createElement("script");
         script.text = `
-setTimeout(() => {
-    yugiohDeckToolApplication.on("ready", () => {
+(function () {
+    function inject() {
         const start = location.search.indexOf("ydke=") + 5;
         const end = location.search.indexOf("&", start);
         const ydke = end === -1 ? location.search.slice(start) : location.search.slice(start, end);
@@ -31,9 +31,30 @@ setTimeout(() => {
         function toPasscodes(base64) {
             return new Uint32Array(Uint8Array.from(atob(base64), byte).buffer)
         }
-    })
-}, 1000)`;
+    }
+    const vueRoot = document.getElementById("deckToolApplication");
+    if (vueRoot) {
+        const observer = new MutationObserver((mutations, invoker) => {
+            for (const mutation of mutations) {
+                if (mutation.type === "childList") {
+                    for (const node of mutation.addedNodes) {
+                        if (node.nodeName === "BUTTON") {
+                            invoker.disconnect();
+                            console.log("Storm Access: button mounted, Vue app API should be available, awaiting ready event");
+                            yugiohDeckToolApplication.on("ready", inject);
+                            return;
+                        }
+                    }
+                }
+            }
+        });
+        console.log("Storm Access: register MutationObserver");
+        observer.observe(vueRoot.parentNode, { childList: true, subtree: true });
+    } else {
+        console.log("Storm Access: no #deckToolApplication, awaiting Vue app ready event");
+        yugiohDeckToolApplication.on("ready", inject);
+    }
+})();`;
         document.body.append(script);
     }
 }
-
