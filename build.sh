5f376add2da2972b7bfdecb49f048338036875cd
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2024-12-07 18:00:16 -0500

    Fix up build script for src directory [ci skip]

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 
 FILES="manifest.json yugioh-card.js ygoprodeck.js COPYING NOTICE icon"
 
+mv ../COPYING ../NOTICE .
+
 zip -r storm-access.zip $FILES
 
 sed -i 's/"manifest_version": 2,/"manifest_version": 3,/' manifest.json
 zip -r storm-access-chrome.zip $FILES
 sed -i 's/"manifest_version": 3,/"manifest_version": 2,/' manifest.json
 
+mv COPYING NOTICE ..
